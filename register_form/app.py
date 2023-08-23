46f72a3c6f8fdf9d202ac61ba3cd0c2747a576ed
Laura-Martinez182 <71099708+Laura-Martinez182@users.noreply.github.com>
2023-08-22 21:17:16 -0500

    Added more fields

--- register_form/app.py
+++ register_form/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, request
 from datetime import datetime
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Email, InputRequired
 
 app = Flask(__name__)
 app.config.from_mapping(
@@ -14,11 +14,18 @@ def index():
 
 #Crear formulario
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, RadioField
+
 
 class RegisterForm(FlaskForm):
-    username = StringField("Username", validators = [DataRequired(), Length(min=4,max=25)])
-    password = PasswordField("Password", validators = [DataRequired(), Length(min=6,max=40)])
+    username = StringField("Username", validators=[DataRequired(), Length(min=4, max=25)])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    name = StringField("Name", validators=[DataRequired()])
+    age = IntegerField("Age", validators=[DataRequired()])
+    gender = RadioField('Gender',
+                        choices=['Female', 'Male', 'Other'],
+                        validators=[InputRequired()])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6, max=40)])
     submit = SubmitField("Register")
 
 @app.route('/auth/register',methods=['GET','POST'])
